82e18978101915f724ad9768a3d8da4fbfe2e72b
shammah <shamaj2006@gmail.com>
2025-07-31 13:32:56 +0100

    display posts

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
 def posts_list(request):
-    return render(request, 'posts/posts_list.html')
+    posts = Post.objects.all()
+    return render(request, 'posts/posts_list.html', {'posts':posts})
